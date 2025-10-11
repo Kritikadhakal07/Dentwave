356f822f2fee630721ca81fddff1a433a45b05ee
pujadhital-kritim <puja.kritim@gmail.com>
2025-10-11 09:38:55 +0545

    about

--- Dentwave/src/App.jsx
+++ Dentwave/src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/user/components/Footer";
 import Header from "./components/user/components/Header";
 import { Routes, Route } from 'react-router-dom';
+import About from "./components/user/Pages/About/About";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
      <Route path = "/" element = {<Header/>} />
      <Route path = "/footer" element = {<Footer/>} />
-
+     <Route path = "/about" element = {<About/>}/>
 
 
     </Routes>
--- Dentwave/src/components/user/Pages/About/About.jsx
+++ Dentwave/src/components/user/Pages/About/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 const About = () => {
   return (
-    <div>hello About</div>
+  <>
+  <Header/>
+  <Footer/>
+  </>
   )
 }
 
