e898b43731982323a2a399a4cdc3ca5d881a7ceb
pujadhital-kritim <puja.kritim@gmail.com>
2025-10-11 08:50:08 +0545

    merge

--- Dentwave/src/App.jsx
+++ Dentwave/src/App.jsx
@@ -1,4 +1,4 @@
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/user/components/Footer";
 import Header from "./components/user/components/Header";
 import { Routes, Route } from 'react-router-dom';
 
